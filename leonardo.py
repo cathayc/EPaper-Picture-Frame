29c9608e62b40fa9a4942d84a0ee77f0780ad9b4
Cathy <cathy.chang.ct@gmail.com>
2023-10-31 19:41:19 -0700

    model

--- leonardo.py
+++ leonardo.py
@@ -16,7 +16,7 @@ def generate_image(prompt):
     # Define the payload for the API request
 	payload = {
 		"height": 512,
-		"modelId": "6bef9f1b-29cb-40c7-b9df-32b51c1f67d3",
+		"modelId": "282ef542-5f20-468e-bce0-54e6423fd5e3",
 		"prompt": prompt,
 		"width": 512
 	}
