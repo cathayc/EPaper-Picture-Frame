d2c8314190a1021f6df1a6d81a2223002a00ab0d
Cathy <cathy.chang.ct@gmail.com>
2023-10-31 18:26:17 -0700

    add functionality to refresh

--- helloworld.py
+++ helloworld.py
@@ -47,7 +47,7 @@ def supported_filetype(file):
     _, ext = os.path.splitext(file)
     return ext.lower() in(".png", ".jpg")
 
-def main(call_leo, random_call_leo):
+def main(call_leo, random_call_leo, refresh_second):
     signal.signal(signal.SIGTERM, exithandler)
     signal.signal(signal.SIGINT, exithandler)
     setup_gpio()  # Set up the GPIO pins
@@ -97,7 +97,7 @@ def main(call_leo, random_call_leo):
 
                 print('Displaying')
                 epd.display(epd.getbuffer(bmp_image))
-                time.sleep(15)
+                time.sleep(refresh_second)
             
             print('Closing...')
             epd.close()
@@ -111,6 +111,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Example of a feature flag in Python")
     parser.add_argument("--call-leo", type=str, help="Call Leonardo AI with the specified prompt")
     parser.add_argument("--random-call-leo", action="store_true", help="Call Leonardo AI with random generated tai and cathy words")
+    parser.add_argument("--refresh-second", type=int, help="Add the number of seconds you'd like the paper to refresh at")
     args = parser.parse_args()
 
-    main(args.call_leo, args.random_call_leo)
+    main(args.call_leo, args.random_call_leo, args.refresh_second)
