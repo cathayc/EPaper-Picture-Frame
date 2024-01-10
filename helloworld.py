fc4c0f111cb988365c63a1fe82b3d7220c113938
Cathy <cathy.chang.ct@gmail.com>
2024-01-09 21:45:37 -0800

    setup imports

--- helloworld.py
+++ helloworld.py
@@ -5,116 +5,13 @@
 import os
 import time
 import sys
-import random
+
 import signal
-import ffmpeg
 import argparse
-import RPi.GPIO as GPIO
-from PIL import Image
-from fractions import Fraction
+
 from leonardo import call_and_save as call_and_save
-from waveshare_epd import epd7in5_V2 as epd_driver
-import random
+from helpers import setup_gpio, display_images, download_images_from_folder
 
-from helpers import download_images_from_folder
-
-def choose_random_words(num_words=5):
-    # Open and read the file containing the list of words
-    with open("word_list.txt", "r") as file:
-        words = file.read().splitlines()
-
-    # Choose 5 random words from the list
-    random_words = random.sample(words, num_words)
-    random_sentence = " ".join(random_words)
-    return random_sentence
-
-# Define the setup_gpio function
-def setup_gpio():
-    # Set up the GPIO pin as an output
-    GPIO.setmode(GPIO.BCM)
-    your_pin_number = 20  # Replace with your actual GPIO pin number
-    GPIO.setup(your_pin_number, GPIO.OUT)
-
-# Define the cleanup_gpio function
-def cleanup_gpio():
-    # Clean up GPIO resources
-    GPIO.cleanup()
-
-def exithandler(signum, frame):
-    try:
-        epd_driver.epdconfig.module_exit()
-    finally:
-        sys.exit()
-
-def supported_filetype(file):
-    _, ext = os.path.splitext(file)
-    return ext.lower() in(".png", ".jpg")
-
-def check_and_delete_images(imgPath):
-    imagedir = os.path.join(os.path.dirname(os.path.realpath(__file__)), imgPath)
-    images = list(filter(supported_filetype, os.listdir(imagedir)))
-    # Check if the number of images exceeds 10
-    if len(images) > 6:
-        # Sort the images by creation time (you may need to implement your own sorting logic)
-        images.sort(key=lambda x: os.path.getctime(os.path.join(imagedir, x)))
-
-        # Delete the earliest 5 images and remove them from the directory
-        for i in range(len(images)-4):
-            # Build the full file path to the image
-            image_path = os.path.join(imagedir, images[i])
-            # Delete the image file
-            os.remove(image_path)
-
-        # Refresh the list of images in t
-
-def display_images(imgPath, refresh_second, loop = False):
-    # Ensure this is the correct path to your video folder
-    imagedir = os.path.join(os.path.dirname(os.path.realpath(__file__)), imgPath)
-    print(imagedir)
-    epd = epd_driver.EPD()
-    width = epd.width
-    height = epd.height
-
-    try:
-        print('Initiating')
-        epd.init()
-        print('Clearing')
-        epd.Clear()
-
-        ordered_images = list(filter(supported_filetype, os.listdir(imagedir)))
-        images = random.sample(ordered_images, len(ordered_images))
-        if not images:
-            print("No images found")
-            sys.exit()
-        
-        # Want this to be looping if the loop = True
-        count = 0
-        while (count < len(images) and not loop) or loop:
-            print(count)
-            # Mod this
-            single_image = images[count % len(images)]
-            # Get current images
-            currentImage = os.path.join(imagedir, single_image)
-            image = Image.open(currentImage)
-            print('Current image name: ', currentImage)
-            image = image.resize((width, height))
-
-            bmp_image = image.convert("RGB")
-            print('Successfully converted to bmp image')
-
-            print('Displaying')
-            epd.display(epd.getbuffer(bmp_image))
-            count= count + 1
-            time.sleep(refresh_second)
-            if count % len(images) == 0:
-                epd.reset()
-        print('Closing...')
-        epd.reset()
-
-    except KeyboardInterrupt:
-        pass
-    finally:
-        cleanup_gpio()  # Clean up the GPIO pins
 
 def main(call_leo, random_call_leo, refresh_second):
     print(f"refresh second: {refresh_second}")
--- helpers.py
+++ helpers.py
@@ -1,5 +1,10 @@
 import requests
 import os
+import random
+import 
+import RPi.GPIO as GPIO
+from PIL import Image
+from waveshare_epd import epd7in5_V2 as epd_driver
 
 from config import dropbox_access_token
 
@@ -58,15 +63,100 @@ def download_images_from_folder(local_destination):
         # Print the response content for non-OK status codes
         print(response.text)
 
-if __name__ == "__main__":
-    # Replace 'dropbox_access_token' with the actual Dropbox access token
-    dropbox_access_token = 'your_dropbox_access_token'
+def choose_random_words(num_words=5):
+    # Open and read the file containing the list of words
+    with open("word_list.txt", "r") as file:
+        words = file.read().splitlines()
 
-    # Replace 'local_destination' with the local directory path where you want to save the downloaded images
-    local_destination = 'local/directory'
+    # Choose 5 random words from the list
+    random_words = random.sample(words, num_words)
+    random_sentence = " ".join(random_words)
+    return random_sentence
 
-    # Create the local destination directory if it doesn't exist
-    os.makedirs(local_destination, exist_ok=True)
+# Define the setup_gpio function
+def setup_gpio():
+    # Set up the GPIO pin as an output
+    GPIO.setmode(GPIO.BCM)
+    your_pin_number = 20  # Replace with your actual GPIO pin number
+    GPIO.setup(your_pin_number, GPIO.OUT)
 
-    # Call the function to download images from the folder
-    download_images_from_folder(local_destination)
+# Define the cleanup_gpio function
+def cleanup_gpio():
+    # Clean up GPIO resources
+    GPIO.cleanup()
+
+def exithandler(signum, frame):
+    try:
+        epd_driver.epdconfig.module_exit()
+    finally:
+        sys.exit()
+
+def supported_filetype(file):
+    _, ext = os.path.splitext(file)
+    return ext.lower() in(".png", ".jpg")
+
+def check_and_delete_images(imgPath):
+    imagedir = os.path.join(os.path.dirname(os.path.realpath(__file__)), imgPath)
+    images = list(filter(supported_filetype, os.listdir(imagedir)))
+    # Check if the number of images exceeds 10
+    if len(images) > 6:
+        # Sort the images by creation time (you may need to implement your own sorting logic)
+        images.sort(key=lambda x: os.path.getctime(os.path.join(imagedir, x)))
+
+        # Delete the earliest 5 images and remove them from the directory
+        for i in range(len(images)-4):
+            # Build the full file path to the image
+            image_path = os.path.join(imagedir, images[i])
+            # Delete the image file
+            os.remove(image_path)
+
+        # Refresh the list of images in t
+
+def display_images(imgPath, refresh_second, loop = False):
+    # Ensure this is the correct path to your video folder
+    imagedir = os.path.join(os.path.dirname(os.path.realpath(__file__)), imgPath)
+    print(imagedir)
+    epd = epd_driver.EPD()
+    width = epd.width
+    height = epd.height
+
+    try:
+        print('Initiating')
+        epd.init()
+        print('Clearing')
+        epd.Clear()
+
+        ordered_images = list(filter(supported_filetype, os.listdir(imagedir)))
+        images = random.sample(ordered_images, len(ordered_images))
+        if not images:
+            print("No images found")
+            sys.exit()
+        
+        # Want this to be looping if the loop = True
+        count = 0
+        while (count < len(images) and not loop) or loop:
+            print(count)
+            # Mod this
+            single_image = images[count % len(images)]
+            # Get current images
+            currentImage = os.path.join(imagedir, single_image)
+            image = Image.open(currentImage)
+            print('Current image name: ', currentImage)
+            image = image.resize((width, height))
+
+            bmp_image = image.convert("RGB")
+            print('Successfully converted to bmp image')
+
+            print('Displaying')
+            epd.display(epd.getbuffer(bmp_image))
+            count= count + 1
+            time.sleep(refresh_second)
+            if count % len(images) == 0:
+                epd.reset()
+        print('Closing...')
+        epd.reset()
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        cleanup_gpio()  # Clean up the GPIO pins
