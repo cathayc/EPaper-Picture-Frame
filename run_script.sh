af0f0f2c3d8937cad0b1a278ae2be62ca878ccd1
Cathy <cathy.chang.ct@gmail.com>
2024-05-28 13:12:40 -0700

    make it home rather than hardcoded cathychang

--- run_script.sh
+++ run_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while true; do
-    source /home/cathychang/AI-Picture-Frame/.venv/bin/activate
-    python3 /home/cathychang/AI-Picture-Frame/helloworld.py
+    source $HOME/AI-Picture-Frame/.venv/bin/activate
+    python3 $HOME/AI-Picture-Frame/helloworld.py
     deactivate
     sleep 300
 done
