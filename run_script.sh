47f68c5d29188441619c92030a478fa9dcd8f4e4
Cathy <cathy.chang.ct@gmail.com>
2025-05-02 12:51:58 -0700

    edit run_script

--- run_script.sh
+++ run_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/cathychang/AI-Picture-Frame
+cd /EP=paper-Picture-Frame
 source .venv/bin/activate
-python main.py --refresh-second 100 > /home/cathychang/frame_script_output.log 2>&1
+python main.py --refresh-second 100 > /home/frame_script_output.log 2>&1
