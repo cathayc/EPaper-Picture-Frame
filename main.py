4a66c6b9a5ab69650a117c72de3c5ad62f38f6e9
Cathy <cathy.chang.ct@gmail.com>
2025-05-02 12:58:27 -0700

    modify main

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import sys
 import signal
 import argparse
 
-from epaper_helpers import setup_gpio, cleanup_gpio, exithandler, display_images
-from epaper_helpers import download_images_from_folder
+from helpers.epaper import setup_gpio, cleanup_gpio, exithandler, display_images
+from helpers.connection import download_images_from_folder
 
 
 def main(refresh_second, folder_path):
